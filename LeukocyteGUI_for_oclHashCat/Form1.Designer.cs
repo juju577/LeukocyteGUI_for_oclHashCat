8d596fc4d808fb1238ec9184ba184e0472e5bdfd
Igor Kotlerman <igor.kotlerman@gmail.com>
2014-11-23 22:17:20 +0200

    Leukocyte GUI for oclHashCat v0.1
    GUI for oclHashCat

---
+++ LeukocyteGUI_for_oclHashCat/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace LeukocyteGUI_for_oclHashCat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelFilesToConvert = new System.Windows.Forms.Label();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chooseConverterDialog = new System.Windows.Forms.OpenFileDialog();
+            this.chooseOutputDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBoxFilenames = new System.Windows.Forms.ListBox();
+            this.buttonMoveDown = new System.Windows.Forms.Button();
+            this.buttonMoveUp = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonChooseOutput = new System.Windows.Forms.Button();
+            this.buttonChooseConverter = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxConverter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelFilesToConvert
+            // 
+            resources.ApplyResources(this.labelFilesToConvert, "labelFilesToConvert");
+            this.labelFilesToConvert.Name = "labelFilesToConvert";
+            // 
+            // buttonConvert
+            // 
+            resources.ApplyResources(this.buttonConvert, "buttonConvert");
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // chooseConverterDialog
+            // 
+            this.chooseConverterDialog.FileName = "aircrack-ng.exe";
+            resources.ApplyResources(this.chooseConverterDialog, "chooseConverterDialog");
+            this.chooseConverterDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.chooseConverterDialog_FileOk);
+            // 
+            // listBoxFilenames
+            // 
+            this.listBoxFilenames.AllowDrop = true;
+            this.listBoxFilenames.FormattingEnabled = true;
+            resources.ApplyResources(this.listBoxFilenames, "listBoxFilenames");
+            this.listBoxFilenames.Name = "listBoxFilenames";
+            this.listBoxFilenames.SelectedIndexChanged += new System.EventHandler(this.listBoxFilenames_SelectedIndexChanged);
+            this.listBoxFilenames.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxFilenames_DragDrop);
+            this.listBoxFilenames.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFilenames_DragEnter);
+            // 
+            // buttonMoveDown
+            // 
+            resources.ApplyResources(this.buttonMoveDown, "buttonMoveDown");
+            this.buttonMoveDown.Image = global::LeukocyteGUI_for_oclHashCat.Properties.Resources.arrow_270_9490;
+            this.buttonMoveDown.Name = "buttonMoveDown";
+            this.buttonMoveDown.UseVisualStyleBackColor = true;
+            this.buttonMoveDown.Click += new System.EventHandler(this.buttonMoveUpDown_Click);
+            // 
+            // buttonMoveUp
+            // 
+            resources.ApplyResources(this.buttonMoveUp, "buttonMoveUp");
+            this.buttonMoveUp.Image = global::LeukocyteGUI_for_oclHashCat.Properties.Resources.arrow_090_8301;
+            this.buttonMoveUp.Name = "buttonMoveUp";
+            this.buttonMoveUp.UseVisualStyleBackColor = true;
+            this.buttonMoveUp.Click += new System.EventHandler(this.buttonMoveUpDown_Click);
+            // 
+            // buttonDelete
+            // 
+            resources.ApplyResources(this.buttonDelete, "buttonDelete");
+            this.buttonDelete.Image = global::LeukocyteGUI_for_oclHashCat.Properties.Resources.cross_4264;
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonChooseOutput
+            // 
+            this.buttonChooseOutput.Image = global::LeukocyteGUI_for_oclHashCat.Properties.Resources.folder_horizontal_open_4182;
+            resources.ApplyResources(this.buttonChooseOutput, "buttonChooseOutput");
+            this.buttonChooseOutput.Name = "buttonChooseOutput";
+            this.buttonChooseOutput.UseVisualStyleBackColor = true;
+            this.buttonChooseOutput.Click += new System.EventHandler(this.buttonChooseOutput_Click);
+            // 
+            // buttonChooseConverter
+            // 
+            this.buttonChooseConverter.Image = global::LeukocyteGUI_for_oclHashCat.Properties.Resources.folder_horizontal_open_4182;
+            resources.ApplyResources(this.buttonChooseConverter, "buttonChooseConverter");
+            this.buttonChooseConverter.Name = "buttonChooseConverter";
+            this.buttonChooseConverter.UseVisualStyleBackColor = true;
+            this.buttonChooseConverter.Click += new System.EventHandler(this.buttonChooseConverter_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::LeukocyteGUI_for_oclHashCat.Properties.Settings.Default, "hccapOutputPath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            resources.ApplyResources(this.textBoxOutput, "textBoxOutput");
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Text = global::LeukocyteGUI_for_oclHashCat.Properties.Settings.Default.hccapOutputPath;
+            // 
+            // textBoxConverter
+            // 
+            this.textBoxConverter.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::LeukocyteGUI_for_oclHashCat.Properties.Settings.Default, "capConverter", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            resources.ApplyResources(this.textBoxConverter, "textBoxConverter");
+            this.textBoxConverter.Name = "textBoxConverter";
+            this.textBoxConverter.Text = global::LeukocyteGUI_for_oclHashCat.Properties.Settings.Default.capConverter;
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonMoveDown);
+            this.Controls.Add(this.buttonMoveUp);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.listBoxFilenames);
+            this.Controls.Add(this.buttonChooseOutput);
+            this.Controls.Add(this.buttonChooseConverter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxConverter);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.labelFilesToConvert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFilesToConvert;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.TextBox textBoxConverter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.OpenFileDialog chooseConverterDialog;
+        private System.Windows.Forms.Button buttonChooseConverter;
+        private System.Windows.Forms.Button buttonChooseOutput;
+        private System.Windows.Forms.FolderBrowserDialog chooseOutputDialog;
+        private System.Windows.Forms.ListBox listBoxFilenames;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonMoveUp;
+        private System.Windows.Forms.Button buttonMoveDown;
+
+
+
+    }
+}
+
---
+++ LeukocyteGUI_for_oclHashCat/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeukocyteGUI_for_oclHashCat
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void listBoxFilenames_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData("FileDrop", false);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                listBoxFilenames.Items.Add(files[i].Replace("\\", "/"));
+            }
+        }
+
+        private void listBoxFilenames_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            //string aircrack = "E:/Program Files (x86)/oclHashcat/cap2hccap/aircrack-ng.exe";
+            //string savePath = "\"E:/Program Files (x86)/oclHashcat/_hccap/\"";
+
+            System.Diagnostics.Process converter = new System.Diagnostics.Process();   
+            converter.StartInfo.FileName = "\"" + textBoxConverter.Text + "\"";
+
+            for (int i = 0; i < listBoxFilenames.Items.Count; i++)
+            {
+                //string file = (listBoxFilenames.Items[i] as string).Replace("\\", "/");
+                string file = listBoxFilenames.Items[i] as string;
+
+                if (file.Length > 4)
+                {
+                    converter.StartInfo.Arguments = '"' + file + '"' + " -J " + "\"" + textBoxOutput.Text + "\""
+                        + System.IO.Path.GetFileNameWithoutExtension(file);
+                    converter.Start();
+                }   
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Properties.Settings.Default.capConverter = textBoxConverter.Text;
+            Properties.Settings.Default.hccapOutputPath = textBoxOutput.Text;
+            Properties.Settings.Default.Save();
+
+            base.OnClosing(e);
+        }
+
+        private void buttonChooseConverter_Click(object sender, EventArgs e)
+        {
+            chooseConverterDialog.ShowDialog();
+        }
+
+        private void buttonChooseOutput_Click(object sender, EventArgs e)
+        {
+            if (chooseOutputDialog.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
+            {
+                textBoxOutput.Text = chooseOutputDialog.SelectedPath + "\\";
+            }
+        }
+
+        private void chooseConverterDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            textBoxConverter.Text = chooseConverterDialog.FileName;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int index = listBoxFilenames.SelectedIndex;
+
+            if(index != -1)
+            {
+                listBoxFilenames.Items.RemoveAt(index);
+            }
+
+            if (listBoxFilenames.Items.Count > index)
+            {
+                listBoxFilenames.SelectedIndex = index;
+            }
+            else if (listBoxFilenames.Items.Count > 0)
+            {
+                listBoxFilenames.SelectedIndex = index - 1;
+            }
+        }
+
+        private void buttonMoveUpDown_Click(object sender, EventArgs e)
+        {
+            int index = listBoxFilenames.SelectedIndex;
+            string buffer;
+
+            if (index == -1) return;
+
+            if ((sender as Button).Name == "buttonMoveUp")
+            {
+                if(index > 0)
+                {
+                    buffer = (string) listBoxFilenames.Items[index - 1];
+                    listBoxFilenames.Items[index - 1] = listBoxFilenames.Items[index];
+                    listBoxFilenames.Items[index] = buffer;
+                    listBoxFilenames.SelectedIndex = index - 1;
+                }
+            }
+            else if(listBoxFilenames.Items.Count > index + 1)
+            {
+                buffer = (string)listBoxFilenames.Items[index + 1];
+                listBoxFilenames.Items[index + 1] = listBoxFilenames.Items[index];
+                listBoxFilenames.Items[index] = buffer;
+                listBoxFilenames.SelectedIndex = index + 1;
+            }
+        }
+
+        private void listBoxFilenames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFilenames.SelectedIndex == -1)
+            {
+                buttonConvert.Enabled  = false;
+                buttonDelete.Enabled   = false;
+                buttonMoveUp.Enabled   = false;
+                buttonMoveDown.Enabled = false;
+            }
+            else
+            {
+                buttonConvert.Enabled  = true;
+                buttonDelete.Enabled   = true;
+                buttonMoveUp.Enabled   = true;
+                buttonMoveDown.Enabled = true;
+            }
+        }
+    }
+}
